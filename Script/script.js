493edc72254cd5b2cea267fcec6ee73e13858910
Your Name <you@example.com>
2023-10-10 14:12:54 +0530

    worked on functionality

---
+++ Script/script.js
@@ -0,0 +1,25 @@
+
+// document.querySelector('.themeOne').addEventListener('click', () => {
+//     document.body.style.backgroundColor = "#3a4764";
+
+// })
+
+
+document.querySelector('.themeTwo').addEventListener('click', () => {
+    document.body.style.backgroundColor = "#e6e6e6";
+    document.querySelector('.roundBox').style.backgroundColor = '#d1cccc';
+    document.querySelector('.displayValue').style.backgroundColor = '#ededed';
+    document.querySelector('.keypadSection').style.backgroundColor = '#d1cccc';
+    const color = document.querySelectorAll('.button').style.color = '#35352c';
+    console.log('color');
+
+})
+
+document.querySelector('.themeThree').addEventListener('click', () => {
+    document.body.style.backgroundColor = "#160628";
+    document.querySelector('.roundBox').style.backgroundColor = '#1d0934';
+    document.querySelector('.displayValue').style.backgroundColor = '#1d0934';
+    document.querySelector('.keypadSection').style.backgroundColor = '#1d0934'
+
+})
+
